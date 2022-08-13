88291f5570344683a020dbb2c70dd964e86b2e48
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-08-13 15:32:45 +0900

    実装完了

--- components/core/Viewport.vue
+++ components/core/Viewport.vue
@@ -1,5 +1,10 @@
 <template lang="pug">
 div
+  Teleport(:to="element")
+    .fullscreen
+      slot(name="2d" :element="element" :time="time" :suspendings="childSuspending2D")
+  .d-none
+    slot(name="3d" :renderer="renderer" :camera="camera" :time="time" :suspengdings="childSuspending3D")
 </template>
 
 <script setup lang="ts">
@@ -12,6 +17,7 @@ const props = withDefaults(
     camera?: Camera;
     time?: number;
     suspending: boolean;
+    topLevel?: boolean;
   }>(),
   {
     route: "/",
@@ -23,4 +29,50 @@ const props = withDefaults(
   }
 );
 const emit = defineEmits<{ (e: "update:suspending", val: boolean): void }>();
+const childSuspending2D = reactive<boolean[]>([]);
+const childSuspending3D = reactive<boolean[]>([]);
+const booleanOr = (input: boolean[]) => {
+  let ret = false;
+  for (const c of input) {
+    if (c) {
+      ret = true;
+      break;
+    }
+  }
+  return ret;
+};
+watch([childSuspending2D, childSuspending3D], () => {
+  const ret = booleanOr([...childSuspending2D, ...childSuspending3D]);
+  emit("update:suspending", ret);
+});
+
+watch(toRef(props, "suspending"), (value) => {
+  if (value) {
+    childSuspending2D.fill(true);
+    childSuspending3D.fill(true);
+  }
+});
+if (props.topLevel) {
+  let unsubscribe: () => void;
+  onMounted(() => {
+    unsubscribe = useRouter().beforeEach((to, from, next) => {
+      childSuspending2D.fill(true);
+      childSuspending3D.fill(true);
+      const unWatch = watch([childSuspending2D, childSuspending3D], () => {
+        const ret = booleanOr([...childSuspending2D, ...childSuspending3D]);
+        if (!ret) {
+          unWatch();
+          next();
+        }
+      });
+    });
+  });
+  onUnmounted(unsubscribe);
+}
 </script>
+
+<style scoped lang="scss">
+.d-none {
+  display: none;
+}
+</style>
--- pages/test/viewport.vue
+++ pages/test/viewport.vue
@@ -1,4 +1,12 @@
 <template lang="pug">
 div
-  CoreViewport()
+  CoreViewport(v-model:suspending="suspending")
+    template(#2d)
+      div hello
+    template(#3d="{renderer,camera,time}")
+      div {{time}}
 </template>
+
+<script setup lang="ts">
+const suspending = ref(false);
+</script>
